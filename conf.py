6d102d830fed4246fe0d4ccb182895b042e4e43a
Ali A Eftekhari <e.eftekhari@gmail.com>
2021-06-10 12:14:36 +0200

    Nikola auto commit.
    Nikola version: 8.1.3

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "Ali A. Eftekhari"  # (translatable)
 BLOG_TITLE = "Subsurface Energy Storage and Extraction"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://subsurfaceenergy.com/"
+SITE_URL = "http://www.subsurfaceenergy.com/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://subsurfaceenergy.com/"
